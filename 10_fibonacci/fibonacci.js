7fd2a8221b05dee501af65a587a8537fec2424ae
DaveZombee <20luky301@gmail.com>
2023-06-17 04:13:02 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    
+    if (member <= 0) {
+        return "OOPS";
+    }
 
+    if (member == 1 || member == 2) {
+        return 1;
+    }
+    
+    lastNum = 1;
+    currentNum = 1;
+
+    currentMember = 2;
+
+    while (currentMember < member) {
+        newNum = lastNum + currentNum;
+        lastNum = currentNum;
+        currentNum = newNum;
+        currentMember++;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
