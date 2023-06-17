3f15228d0ed306b6d5401235bf9f050ad73b657a
DaveZombee <20luky301@gmail.com>
2023-06-17 03:01:03 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let result = [];
+    for (let item of arr) {
+        if (!remove.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
