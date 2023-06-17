75e225495cd7a015efb16c3ddf6a7f51029ca903
DaveZombee <20luky301@gmail.com>
2023-06-17 04:37:54 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = people.map(getAge);
+    let maxAge = Math.max(...ages);
+    let maxIndex = ages.indexOf(maxAge);
 
+    return people[maxIndex];
 };
+
+const getAge = function(person) {
+    if (person.yearOfDeath === undefined) {
+        let currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
